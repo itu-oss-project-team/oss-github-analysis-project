21c7e794949d1c93893ccabb9bd972a06863f185
erensahin <sahineren.09@gmail.com>
2017-01-24 23:15:22 +0300

    Refactored analysis.py

--- github-parser/analysis.py
+++ github-parser/analysis.py
@@ -9,30 +9,34 @@ class Analysis:
         # Generate a github_requester with imported GitHub tokens
         self.__databaseService = DatabaseService(secret_config['mysql'])
 
-    def setMonthlyProjectStats(self):
+    def setMonthlyProjectStats(self, repo_id):
         start_date = dateutil.parser.parse("2013-01-01 00:00:00")
-        end_date = dateutil.parser.parse("2017-01-01 00:00:00")
+        end_date = dateutil.parser.parse("2017-02-01 00:00:00")
 
-        repos = self.__databaseService.getAllRepos(get_only_ids=True)
-        for repo in repos:
-            self.__databaseService.findNumberOfCommitsAndContributorsOfProjectMonthly(repo, start_date, end_date)
-
+        self.__databaseService.findNumberOfCommitsAndContributorsOfProjectMonthly(repo_id, start_date, end_date)
         return
 
-    def setFileStats(self):
-        repos = self.__databaseService.getAllRepos(get_only_ids=True)
-        for repo in repos:
-            self.__databaseService.findNumberOfCommitsAndDevelopersOfRepositoryFiles(repo)
+    def setFileStats(self, repo_id):
 
+        self.__databaseService.findNumberOfCommitsAndDevelopersOfRepositoryFiles(repo_id)
         return
+
+    def getRepos(self):
+        repos = self.__databaseService.getAllRepos(get_only_ids=True)
+        return repos
 
 def main():
     with open(os.path.join(os.path.dirname(__file__), os.pardir, 'config_secret.yaml'), 'r') as ymlfile:
         secret_config = yaml.load(ymlfile)
 
     analysis = Analysis(secret_config)
-    #analysis.setMonthlyProjectStats()
-    analysis.setFileStats()
+    repos = analysis.getRepos()
+    for repo in repos:
+        analysis.setMonthlyProjectStats(repo)
+        analysis.setFileStats(repo)
+
+    print("Monthly project stats and file stats analyses are done.")
+    return
 
 main()
 
