84dc273aa1e046eb37a950e927fd9116b81920ed
Kubilay Karpat <karpatkubilay@gmail.com>
2017-02-20 12:40:49 +0300

    Parameters now will be given as command line arguments

--- github-parser/__main__.py
+++ github-parser/__main__.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import yaml
+import getopt
 import os.path
+import sys
+import yaml
 
 from harvester import GitHubHarvester
 
 
-def main():
+def main(argv):
+    since = None
+    until = None
+    star = 10000
+    force = False
+    try:
+        opts, args = getopt.getopt(argv, "hu:s:f", ["since=", "until=", "star=", "force"])
+    except getopt.GetoptError:
+        print('ERROR in input arguments!')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':  # Help case
+            print('__main.py__ -s <since_date> -u <until_date> --star <min_star_number> -f')
+            sys.exit()
+        elif opt in ("-s", "--since"):
+            since = arg
+        elif opt in ("-u", "--until"):
+            until = arg
+        elif opt in ("--star"):
+            star = arg
+        elif opt in ("-f", "--force"):
+            force = True
 
     with open(os.path.join(os.path.dirname(__file__), os.pardir, 'config.yaml'), 'r') as ymlfile:
         config = yaml.load(ymlfile)
@@ -15,7 +38,8 @@ def main():
         secret_config = yaml.load(ymlfile)
 
     github_harvester = GitHubHarvester(config, secret_config)
-    github_harvester.fetchRepos("10000", "2016-01-01T00:00:00Z", "2017-04-01T00:00:00Z", True)
+    github_harvester.fetchRepos(star, since, until, force)
+    #github_harvester.fetchRepos("10000", "2016-01-01T00:00:00Z", "2017-04-01T00:00:00Z", True)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
