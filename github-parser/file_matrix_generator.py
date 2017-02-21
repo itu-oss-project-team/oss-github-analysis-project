e7c4cf52b3ab6eace617e50071a490c8656c1c5b
erensahin <sahineren.09@gmail.com>
2017-02-21 20:59:17 +0300

    Converted matrix to csv file. seperating with ; now.

--- github-parser/file_matrix_generator.py
+++ github-parser/file_matrix_generator.py
@@ -1,5 +1,8 @@
 from database_service import DatabaseService
 from db_coloumn_constants import Coloumns
+import os.path
+import sys
+import yaml
 
 class FileMatrixGenerator:
 
@@ -21,16 +24,16 @@ class FileMatrixGenerator:
                     # For every files changed togehter
                     self.__increment_commit_count(file_matrix, file_path_1, file_path_2)
 
-        with open(str(repo_id) + "_filematrix.txt", "w") as out_file:
+        with open(str(repo_id) + "_filematrix.csv", "w") as out_file:
             for file in repo_files:
-                out_file.write("%s\n" % file)
+                out_file.write("%s;" % file)
             out_file.write("\n")
             for file_1 in repo_files:
                 for file_2 in repo_files:
                     if not file_2 in file_matrix[file_1]:
-                        out_file.write("%3d " % 0)
+                        out_file.write("%d;" % 0)
                     else:
-                        out_file.write("%3d " % file_matrix[file_1][file_2])
+                        out_file.write("%d;" % file_matrix[file_1][file_2])
                 out_file.write("\n")
 
     def __increment_commit_count(self, file_matrix, file_path_1, file_path_2):
@@ -40,4 +43,3 @@ class FileMatrixGenerator:
             file_matrix[file_path_1][file_path_2] += 1
         else:
             file_matrix[file_path_1][file_path_2] = 1
-
