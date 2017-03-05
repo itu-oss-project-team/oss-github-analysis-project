d09efa23e2b9921811068e334786cf70defc246b
erensahin <sahineren.09@gmail.com>
2017-03-05 13:50:05 +0300

    Changed main back to the version where parameters are given as command line arguments.

--- github-parser/__main__.py
+++ github-parser/__main__.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import yaml
+import getopt
 import os.path
+import sys
+import yaml
 
 from harvester import GitHubHarvester
-from generate_matrices import GenerateMatrices
 
-def main():
+
+def main(argv):
+    since = None
+    until = None
+    star = 10000
+    force = False
+    try:
+        opts, args = getopt.getopt(argv, "hu:s:f", ["since=", "until=", "star=", "force"])
+    except getopt.GetoptError:
+        print('ERROR in input arguments!')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':  # Help case
+            print('__main.py__ -s <since_date> -u <until_date> --star <min_star_number> -f')
+            sys.exit()
+        elif opt in ("-s", "--since"):
+            since = arg
+        elif opt in ("-u", "--until"):
+            until = arg
+        elif opt in ("--star"):
+            star = arg
+        elif opt in ("-f", "--force"):
+            force = True
 
     with open(os.path.join(os.path.dirname(__file__), os.pardir, 'config.yaml'), 'r') as ymlfile:
         config = yaml.load(ymlfile)
@@ -14,17 +37,9 @@ def main():
     with open(os.path.join(os.path.dirname(__file__), os.pardir, 'config_secret.yaml'), 'r') as ymlfile:
         secret_config = yaml.load(ymlfile)
 
-    #github_harvester = GitHubHarvester(config, secret_config)
-    #github_harvester.fetchRepos("40000", "2016-01-01T00:00:00Z", None, True)
-    
-    #github_harvester.fetchRepo("itu-oss-project-team","oss-github-analysis-project", "2016-01-01T00:00:00Z", None, True)
-    
-    
-    matrix_gen = GenerateMatrices(secret_config)
-    matrix_gen.create_matrices(secret_config)
-    
+    github_harvester = GitHubHarvester(config, secret_config)
+    github_harvester.fetchRepos(star, since, until, force)
     #github_harvester.fetchRepos("10000", "2016-01-01T00:00:00Z", "2017-04-01T00:00:00Z", True)
 
-    
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
