bb82358bcb30c992b5922deb8af7c73c6901d2d4
Eren Şahin <sahineren.09@gmail.com>
2017-03-30 23:34:48 +0300

    Fixed OUTPUT_DIRS

--- github_analysis_tool/__init__.py
+++ github_analysis_tool/__init__.py
@@ -3,7 +3,7 @@ import yaml
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-OUTPUT_DIR = os.path.join(ROOT_DIR, os.pardir, "outputs")
+OUTPUT_DIR = os.path.join(ROOT_DIR , "outputs")
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 
